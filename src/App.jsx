d59b392875691638661e756cf76cbfd16133ef8f
Oswaldo OR <L22010105@orizaba.tecnm.mx>
2025-10-22 18:06:14 -0600

    Hola Es Otra Prueba

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Oswaldo Oltehua",
+  title: "Ing Sistemas",
+  email: "L22010105@orizaba.tecnm.mx",
+  gitHub: "OswaldoOR",
+  instagram: "oswaldo.or",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "oswaldo.or",
+  youTube: "OswaldoOR",
 };
 
 const primaryColor = "#4E567E";
